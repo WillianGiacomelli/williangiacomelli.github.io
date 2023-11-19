18c095bebc280374cc313263206b4fe8732e92e3
Willian Giacomelli <willian2406@live.com>
2023-11-19 16:06:09 -0300

    header finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const btnMobile = document.querySelector("#barra");
+const fecharMobile = document.querySelector("#fechar");
+
+function toggleMenu() {
+  const nav = document.querySelector(".menu");
+  nav.classList.toggle("active");
+  const ativo = nav.classList.contains("active");
+  if (ativo) {
+    fecharMobile.classList.toggle("active");
+    btnMobile.classList.add("remove");
+  } else {
+    btnMobile.classList.remove("remove");
+    fecharMobile.classList.remove("active");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+fecharMobile.addEventListener("click", toggleMenu);
+
+const menu = document.querySelector(".menu-container");
+const intro = document.querySelector(".intro");
+
+window.addEventListener("scroll", stickyFunc);
+
+var navbar = document.querySelector(".header-container");
+
+var sticky = navbar.offsetTop;
+
+console.log(sticky);
+
+function stickyFunc() {
+  if (window.scrollY >= sticky + 5) {
+    menu.classList.add("sticky");
+    intro.style.paddingTop = "120px";
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
